f0dae748bbfe614d0ff3173d7762bb93a1c7cfd0
Jorge <ogeandes1974@gmail.com>
2025-02-17 17:54:46 -0500

    added basic style, modularized solve drawing

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-const maze_width = 29;
-const maze_height = 29;
 var canvas = document.getElementById("lab_canvas");
+var ctx = canvas.getContext("2d");
+
+const maze_width = 43;
+const maze_height = 43;
+const cell_width = Math.floor(canvas.width/maze_width);
+const cell_height = Math.floor(canvas.height/maze_height);
 
 var maze = [];
 
-canvas.addEventListener("click", canvasClick);
+canvas.onclick = canvasClick;
+//canvas.onkeydown = canvasKeyDown;
+
+function canvasKeyDown(event){
+    console.log("keyddown");
+}
 
 function canvasClick(event){
-    var cell_width = Math.floor(canvas.width/maze_width);
-    var cell_height = Math.floor(canvas.height/maze_height);
     var mazeX = Math.ceil(event.offsetX/cell_width)-1;
     var mazeY = Math.ceil(event.offsetY/cell_height)-1;
 
+    //starting cell
     maze[1][0] = 2;
-    var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "green";
-        ctx.fillRect(cell_width, 0,
-            cell_width, cell_height);
-    var action = userSolveCell(mazeX, mazeY);
+    ctx.fillStyle = "green";
+    ctx.fillRect(cell_width, 0,
+        cell_width, cell_height);
+
+    userSolveCell(mazeX, mazeY);
+}
+
+function userSolveCell(x, y){
+    //console.log(x + "," + y); 
+    if(maze[x][y] == 0 && (maze[x-1][y] == 2 || maze[x][y-1] == 2 || maze[x+1][y] == 2 || maze[x][y+1] == 2)){
+        maze[x][y] = 2;
+        userSolveDraw(x, y, 1);
+        return;
+    }
+    if(maze[x][y] == 2){
+        maze[x][y] = 0;
+        userSolveDraw(x, y, 2);
+    }
+}
+
+function userSolveDraw(mazeX, mazeY, action){
+    //console.log("drawing " + mazeX + "," + mazeY);
     if(action == 1){
+        //console.log("action 1")
+        ctx.fillStyle = "green";
         ctx.fillRect(cell_width * mazeX, cell_height * mazeY,
             cell_width, cell_height);
     } else if(action == 2){
+        //console.log("action 2")
         ctx.fillStyle = "white";
         ctx.fillRect(cell_width * mazeX, cell_height * mazeY,
             cell_width, cell_height);
@@ -29,22 +57,8 @@ function canvasClick(event){
     ctx.fillStyle = "black";
 }
 
-function userSolveCell(x, y){
-//    console.log(x + "," + y); 
-    if(maze[x][y] == 0 && (maze[x-1][y] == 2 || maze[x][y-1] == 2 || maze[x+1][y] == 2 || maze[x][y+1] == 2)){
-        maze[x][y] = 2;
-        return 1;
-    }
-    if(maze[x][y] == 2){
-        maze[x][y] = 0;
-        return 2;
-    }
-}
-
 //modified from https://github.com/professor-l/mazes
 function drawMaze() {
-    var ctx = canvas.getContext("2d");
-    
     var rectWidth = Math.floor(canvas.width / maze[0].length);
     var rectHeight = Math.floor(canvas.height / maze.length);
     
@@ -72,7 +86,6 @@ function backtracking(){
        3
 */
 function backtrackingMaze(width, height){
-    //copied from https://github.com/professor-l/mazes
     maze = [];
     for (var i = 0; i < height; i++) {
         maze.push([]);
@@ -181,10 +194,6 @@ function backtrackingMaze(width, height){
 
     drawMaze();
     //printMaze(maze);
-}
-
-function shufflePos(maze, pos_stack){
-    return pos_stack.pop();
 }
 
 function printMaze(){
